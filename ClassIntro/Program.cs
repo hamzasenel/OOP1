be56086320309c978ef6860f23116aa93d1084e0
Hamza Şenel <hamza.senel@anlas.com>
2021-01-28 14:42:12 +0300

    Proje dosyası ekle.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs() { egitmen="hamzasenel11111111111",kursAdi="turkce",izlenmeOrani=68};
+            Kurs kurs2 = new Kurs() { egitmen = "hamzasenel2222", kursAdi = "matematik", izlenmeOrani = 98 };
+            Kurs kurs3 = new Kurs() { egitmen = "hamzasenel3333", kursAdi = "english", izlenmeOrani = 78 };
+            Kurs[] kurslar = new Kurs[] { kurs1,kurs2,kurs3};
+            foreach (Kurs item in kurslar)
+            {
+                Console.WriteLine(item.egitmen+item.izlenmeOrani+item.kursAdi);
+            }
+        }
+    }
+    class Kurs
+    {
+        public string kursAdi { get; set; }
+        public string egitmen { get; set; }
+        public int izlenmeOrani { get; set; }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String[] dizi = new String[]{ "asd","asd","fad"};
+            Console.WriteLine("Hello World!");
+            Product product1 = new Product { Id = 12,CategoryId=2, ProductName = "huhs",UnitPrice=3,UnitInStock=500 };
+            ProductManager productManager = new ProductManager(); Console.WriteLine("-----");
+            productManager.Add(product1); Console.WriteLine("-----");
+            
+            Console.WriteLine();
+            foreach (string item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+            int[] sayilar1 = new int[] {1,2,3 };
+            int[] sayilar2 = new int[] { 10,20,30};
+            sayilar1 = sayilar2;
+            //sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]);
+        }
+    }
+}
